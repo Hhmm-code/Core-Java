9b07b2ea0c52f877170389aa73564f2454212ab7
Hhmm-code <phhari10.sify@gmail.com>
2025-04-03 12:30:10 +0530

    ForEachLoop.java

---
+++ Loop/ForEachLoop.java
@@ -0,0 +1,20 @@
+
+/*
+			For each
+		for (int i:Arr1)
+			
+	for each lop can be used only for array 
+	Syntax :
+			 for( datatype variable : arrayVariable)
+*/
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		int Arr1[]={1,0,3,4};
+		for (int i:Arr1){
+		    System.out.println("Arr1 "+i);
+		}
+	}
+}
