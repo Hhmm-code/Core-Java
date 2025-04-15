862f789e0437ad42db663b4dcd18ce08ff60f672
Hhmm-code <phhari10.sify@gmail.com>
2025-04-15 15:54:09 +0530

    Array property

--- Array/1.sum of array.java
+++ Array/1.sum of array.java
@@ -20,3 +20,28 @@ public class Main
 }
 
 /
+
+
+// Array
+import java.util.Arrays;
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Array");
+        int [] arr1= new int[5];
+        arr1[0]=5;
+        arr1[1]=2;
+        arr1[3]=5;
+        arr1[4]=3;
+        
+        //1. to print array
+        System.out.println(Arrays.toString(arr1));   // it is a static method of Arrays class
+        
+        for( int i =0;i<arr1.length;i++)
+        {
+            System.out.print(arr1[i]);
+        }
+        System.out.println();
+        //2. lenth of array
+        System.out.println(arr1.length); //
+    }
+}
